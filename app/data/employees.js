a3b13241472a412df95c8accdcf437511b0dd3a3
Chaiku <46286216+Chaiku@users.noreply.github.com>
2019-02-16 01:11:31 -0500

    updated employee array

--- app/data/employees.js
+++ app/data/employees.js
@@ -9,6 +9,46 @@ var employeeArray = [
     empName: "Louise",
     // empPic:<img src="*"></img>,
     empSum: 10
+    },
+    {
+        empName:"Francis",
+        //empPic
+        empSum: 15
+    },
+    {
+        empName: "Arnold",
+        //empPic
+        empSum: 20
+    },
+    {
+        empName: "Miranda",
+        //empPic
+        empSum: 25
+    },
+    {
+        empName: "Bertha",
+        //empPic
+        empSum: 30
+    },
+    {
+        empName: "Frank",
+        //empPic
+        empSum: 35
+    },
+    {
+        empName: "Wanda",
+        //empPic
+        empSum: 40
+    },
+    {
+        empName: "Gerome",
+        //empPic
+        empSum: 45
+    },
+    {
+        empName: "Andrea",
+        //empPic
+        empSum: 50
     }
 
 ];
