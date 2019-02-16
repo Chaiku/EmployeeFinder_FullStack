faa019f7af1f57a3f63d472f8762a20f293327e5
Chaiku <46286216+Chaiku@users.noreply.github.com>
2019-02-16 14:03:42 -0500

    testing form submit functionality

--- app/data/employees.js
+++ app/data/employees.js
@@ -11,44 +11,44 @@ var employeeArray = [
     empSum: 10
     },
     {
-        empName:"Francis",
-        //empPic
-        empSum: 15
+    empName:"Francis",
+    //empPic
+    empSum: 15
     },
     {
-        empName: "Arnold",
-        //empPic
-        empSum: 20
+    empName: "Arnold",
+    //empPic
+    empSum: 20
     },
     {
-        empName: "Miranda",
-        //empPic
-        empSum: 25
+    empName: "Miranda",
+    //empPic
+    empSum: 25
     },
     {
-        empName: "Bertha",
-        //empPic
-        empSum: 30
+    empName: "Bertha",
+    //empPic
+    empSum: 30
     },
     {
-        empName: "Frank",
-        //empPic
-        empSum: 35
+    empName: "Frank",
+    //empPic
+    empSum: 35
     },
     {
-        empName: "Wanda",
-        //empPic
-        empSum: 40
+    empName: "Wanda",
+    //empPic
+    empSum: 40
     },
     {
-        empName: "Gerome",
-        //empPic
-        empSum: 45
+    empName: "Gerome",
+    //empPic
+    empSum: 45
     },
     {
-        empName: "Andrea",
-        //empPic
-        empSum: 50
+    empName: "Andrea",
+    //empPic
+    empSum: 50
     }
 
 ];
--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,6 +1,20 @@
-// $('#submitBtn').on("click", completeSurvey) 
+$('form').on("submit", function(event) {
+    event.preventDefault();
+    console.log($(this).serialize() );
+}) ;
 
-// const completeSurvey = function () {
-//     let answer1 = 2;
-//     return answer1;
-// }
+
+
+
+let answerArray = [];
+let surveyAnswers = []
+
+const completeSurvey = function () {
+for(i = 0; i < surveyAnswers.length; i++) {
+    answerAray.push(question[i].val());
+    
+}
+
+for ( i = 0; i < answerArray.length; i++) {
+    math.abs(answerArray[i] - employeeAnswer[i])
+}
